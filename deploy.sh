5ff03cf60012540652aeb3f5e9358f9d7485062a
lzxb <1340641314@qq.com>
2025-03-25 12:35:01 +0800

    refactor(deploy.sh): 移除部署步骤并添加pnpm安装
    移除dist目录下的git初始化和部署步骤，添加pnpm安装命令以优化依赖管理

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 rm -rf dist
+npx pnpm@10 install
 # 编译代码
 npm run build
 
@@ -32,15 +33,15 @@ if [ -f "$src_base/docs/doc_build/sitemap.xml" ]; then
   echo "Copied sitemap.xml to $target_base"
 fi
 
-cd dist
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# cd dist
+# # 如果是发布到自定义域名
+# # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-# 如果发布到 https://js-esm.github.io/gez/
-git push -f git@github.com:js-esm/gez.git master:docs
+# # 如果发布到 https://js-esm.github.io/gez/
+# git push -f git@github.com:js-esm/gez.git master:docs
 
 cd -
