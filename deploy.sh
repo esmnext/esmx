098e1a98567643dd284a7637f497550c0214227a
lzxb <1340641314@qq.com>
2025-04-02 15:51:22 +0800

    ci(deploy): 使用GITHUB_TOKEN进行安全部署
    将部署脚本中的SSH推送方式改为使用GITHUB_TOKEN进行HTTPS推送，以提高安全性并避免SSH密钥管理的复杂性。

--- deploy.sh
+++ deploy.sh
@@ -47,6 +47,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:esmnext/esmx.git main:docs
+git push -f https://${GITHUB_TOKEN}@github.com/esmnext/esmx.git main:docs
 
 cd -
