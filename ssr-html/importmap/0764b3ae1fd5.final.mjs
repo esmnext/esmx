0ae619aeb7d1a03772973a4669a3a392860646e4
lzxb <1340641314@qq.com>
2025-05-22 08:23:49 +0000

    Deploying to docs from @ esmnext/esmx@8e8291877a2a31de3fdb2ff4e15311b3aa9e13f6 🚀

--- ssr-html/importmap/0764b3ae1fd5.final.mjs
+++ ssr-html/importmap/0764b3ae1fd5.final.mjs
@@ -1,6 +1,6 @@
 (() => {
 const base = document.currentScript.getAttribute("data-base");
-const importmap = {"scopes":{},"imports":{"ssr-html\u002Fsrc\u002Fentry.client":"\u002Fssr-html\u002Fsrc\u002Fentry.client.d731ede7.final.mjs","ssr-html\u002Fsrc\u002Ftitle\u002Findex":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs","ssr-html\u002Fsrc\u002Ftitle":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs"}};
+const importmap = {"imports":{"ssr-html\u002Fsrc\u002Fentry.client":"\u002Fssr-html\u002Fsrc\u002Fentry.client.d731ede7.final.mjs","ssr-html\u002Fsrc\u002Ftitle\u002Findex":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs","ssr-html\u002Fsrc\u002Ftitle":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs"},"scopes":{"\u002Fssr-html\u002F":{}}};
 if (importmap.imports && base) {
     const imports = importmap.imports;
     Object.entries(imports).forEach(([k, v]) => {
