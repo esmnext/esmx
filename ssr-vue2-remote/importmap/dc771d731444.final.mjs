1dff5e89473aeaf9538c83075edaa7910cc39ebd
lzxb <1340641314@qq.com>
2025-05-22 04:17:10 +0000

    Deploying to docs from @ esmnext/esmx@2531beb5fc6d22942b9fadf5911028acad000179 🚀

---
+++ ssr-vue2-remote/importmap/dc771d731444.final.mjs
@@ -0,0 +1,14 @@
+(() => {
+const base = document.currentScript.getAttribute("data-base");
+const importmap = {"scopes":{"\u002Fssr-vue2-remote\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs"}},"imports":{"ssr-vue2-remote\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-remote\u002Fsrc\u002Fentry.client.60b13bfc.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.fc7fb2bf.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.3137e5b1.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.fc7fb2bf.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.3137e5b1.final.mjs"}};
+if (importmap.imports && base) {
+    const imports = importmap.imports;
+    Object.entries(imports).forEach(([k, v]) => {
+        imports[k] = base + v;
+    });
+}
+const script = document.createElement("script");
+script.type = "importmap";
+script.innerHTML = JSON.stringify(importmap);
+document.head.appendChild(script);
+})();
