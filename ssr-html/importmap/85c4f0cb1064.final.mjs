4b9bff82ee22dabda5292562af954bb9e672e35e
lzxb <1340641314@qq.com>
2025-05-23 06:24:24 +0000

    Deploying to docs from @ esmnext/esmx@5778637a48853e75bf15ca5ee431cb240b80c579 🚀

--- ssr-html/importmap/85c4f0cb1064.final.mjs
+++ ssr-html/importmap/85c4f0cb1064.final.mjs
@@ -1,14 +1,18 @@
 (() => {
 const base = document.currentScript.getAttribute("data-base");
 const importmap = {"imports":{"ssr-html\u002Fsrc\u002Fentry.client":"\u002Fssr-html\u002Fsrc\u002Fentry.client.759ee756.final.mjs","ssr-html\u002Fsrc\u002Ftitle\u002Findex":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs","ssr-html\u002Fsrc\u002Ftitle":"\u002Fssr-html\u002Fsrc\u002Ftitle\u002Findex.2d79c0c2.final.mjs"},"scopes":{"\u002Fssr-html\u002F":{}}};
-if (importmap.imports && base) {
-    const imports = importmap.imports;
-    Object.entries(imports).forEach(([k, v]) => {
-        imports[k] = base + v;
+const set = (data) => {
+    if (!data) return;
+    Object.entries(data).forEach(([k, v]) => {
+        data[k] = base + v;
     });
+};
+set(importmap.imports);
+if (importmap.scopes) {
+    Object.values(importmap.scopes).forEach(set);
 }
 const script = document.createElement("script");
 script.type = "importmap";
-script.innerHTML = JSON.stringify(importmap);
+script.innerText = JSON.stringify(importmap);
 document.head.appendChild(script);
 })();
