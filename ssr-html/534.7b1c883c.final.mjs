2861eb5a099c0ac080b3fdcd1a80a6c8cce0d3cf
lzxb <1340641314@qq.com>
2025-05-27 12:32:06 +0000

    Deploying to docs from @ esmnext/esmx@4d0e88d41999cfec0bbcb446abedcd20939dda9f 🚀

---
+++ ssr-html/534.7b1c883c.final.mjs
@@ -0,0 +1,196 @@
+export const __webpack_ids__ = ['534'];
+export const __webpack_modules__ = {
+878: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.d(__webpack_exports__, {
+  b: () => (layout)
+});
+/* ESM import */var _layout_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(992);
+
+function layout(slot) {
+    let options = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : {};
+    const { base = '/' } = options;
+    // 判断链接是否激活
+    const isActive = (path)=>{
+        // 对于空路径的特殊处理
+        if (!path) {
+            return '';
+        }
+        // 如果 options.url 不存在，返回空字符串
+        if (!options.url) {
+            return '';
+        }
+        // 从 URL 中移除查询参数
+        const urlWithoutQuery = options.url.split('?')[0];
+        const pathWithoutQuery = path.startsWith('/') ? path : '/' + path;
+        // 对于首页的特殊处理
+        if (pathWithoutQuery === '/' && urlWithoutQuery === '/') {
+            return 'active';
+        }
+        return urlWithoutQuery === pathWithoutQuery ? 'active' : '';
+    };
+    // 处理相对路径
+    const resolvePath = (path)=>{
+        if (path.startsWith('http')) return path;
+        return base + (path.startsWith('/') ? path.slice(1) : path);
+    };
+    return '\n<div class="layout">\n    <header class="header">\n        <div class="container">\n            <h1><img src="https://www.esmnext.com/logo.svg" alt="Esmx Logo" width="48" height="48"></h1>\n            <nav class="nav">\n                <a href="'.concat(resolvePath('/'), ' " class="').concat(isActive('/'), '">首页</a>\n                <a href="').concat(resolvePath('about'), '" class="').concat(isActive('/about'), '">关于我们</a>\n                <a href="https://github.com/esmnext/esmx/tree/master/examples/ssr-html" target="_blank">示例代码</a>\n            </nav>\n        </div>\n    </header>\n    <main class="main">\n        <div class="container">\n            ').concat(slot, "\n        </div>\n    </main>\n</div>\n");
+}
+
+
+}),
+286: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.d(__webpack_exports__, {
+  T: () => (Page)
+});
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+class Page {
+    get props() {
+        if (this._props === null) {
+            throw new Error("props is null");
+        }
+        return this._props;
+    }
+    set props(props) {
+        this._props = props;
+    }
+    /**
+     * 服务端渲染生成的 HTML
+     */ render() {
+        return "";
+    }
+    /**
+     * 组件已经创建完成，props 和 state 已经准备就绪
+     */ onCreated() {}
+    /**
+     * 客户端执行
+     */ onClient() {}
+    /**
+     * 服务端执行
+     */ async onServer() {
+        this.importMetaSet.add(import.meta);
+    }
+    constructor(){
+        _define_property(this, "importMetaSet", new Set());
+        _define_property(this, "_props", null);
+        _define_property(this, "title", '');
+        /**
+     * 自定义页面状态
+     */ _define_property(this, "state", {});
+    }
+}
+
+
+}),
+40: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: ./src/components/layout.ts
+var layout = __webpack_require__(878);
+;// CONCATENATED MODULE: ./src/images/cat.jpeg
+const cat_namespaceObject = __webpack_require__.p + "images/cat.ed79ef6b.final.jpeg";
+;// CONCATENATED MODULE: ./src/images/logo.svg
+const logo_namespaceObject = __webpack_require__.p + "images/logo.3923d727.final.svg";
+;// CONCATENATED MODULE: ./src/images/running-dog.gif
+const running_dog_namespaceObject = __webpack_require__.p + "images/running-dog.76197e20.final.gif";
+;// CONCATENATED MODULE: ./src/images/starry.jpg
+const starry_namespaceObject = __webpack_require__.p + "images/starry.d914a632.final.jpg";
+;// CONCATENATED MODULE: ./src/images/sun.png
+const sun_namespaceObject = __webpack_require__.p + "images/sun.429a7bc5.final.png";
+;// CONCATENATED MODULE: ./src/images/index.ts
+
+
+
+
+
+
+
+// EXTERNAL MODULE: ./src/page.ts
+var page = __webpack_require__(286);
+// EXTERNAL MODULE: external "ssr-html/src/title/index"
+var index_ = __webpack_require__(360);
+;// CONCATENATED MODULE: ./src/views/home.ts
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+
+
+
+
+class Home extends page/* Page */.T {
+    render() {
+        const { url, base } = this.props;
+        const { count } = this.state;
+        return (0,layout/* layout */.b)('\n        <section>\n            <h2>计数器</h2>\n            <div class="content-area counter">\n                <div id="count" class="counter-value">'.concat(count, '</div>\n            </div>\n        </section>\n\n        <section>\n            <h2>请求地址</h2>\n            <div class="content-area url-section">\n                <pre>').concat(url, '</pre>\n            </div>\n        </section>\n\n        <section>\n            <h2>图片展示</h2>\n            <ul class="image-grid">\n                <li>\n                    <div class="image-wrapper">\n                        <img src="').concat(logo_namespaceObject, '" alt="SVG示例" width="200" height="200">\n                    </div>\n                    <div class="image-info">\n                        <h3>SVG 示例</h3>\n                        <p>类型：SVG</p>\n                        <p>尺寸：200 x 200</p>\n                    </div>\n                </li>\n                <li>\n                    <div class="image-wrapper">\n                        <img src="').concat(starry_namespaceObject, '" alt="JPG示例" width="1024" height="768">\n                    </div>\n                    <div class="image-info">\n                        <h3>JPG 示例</h3>\n                        <p>类型：JPG</p>\n                        <p>尺寸：1024 x 768</p>\n                    </div>\n                </li>\n                <li>\n                    <div class="image-wrapper">\n                        <img src="').concat(cat_namespaceObject, '" alt="猫咪图片" width="769" height="225">\n                    </div>\n                    <div class="image-info">\n                        <h3>猫咪图片</h3>\n                        <p>类型：PNG</p>\n                        <p>尺寸：769 x 225</p>\n                    </div>\n                </li>\n                <li>\n                    <div class="image-wrapper">\n                        <img src="').concat(running_dog_namespaceObject, '" alt="疯狂编码" width="480" height="297">\n                    </div>\n                    <div class="image-info">\n                        <h3>疯狂编码</h3>\n                        <p>类型：GIF</p>\n                        <p>尺寸：480 x 297</p>\n                    </div>\n                </li>\n                <li>\n                    <div class="image-wrapper">\n                        <img src="').concat(sun_namespaceObject, '" alt="太阳图标" width="351" height="300">\n                    </div>\n                    <div class="image-info">\n                        <h3>太阳图标</h3>\n                        <p>类型：SVG</p>\n                        <p>尺寸：351 x 300</p>\n                    </div>\n                </li>\n            </ul>\n        </section>\n\n        <section class="update-section">\n            <div class="update-info">\n                <span>最后更新：').concat(new Date(this.state.time).toLocaleString('zh-CN', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        }), "</span>\n            </div>\n        </section>\n"), {
+            url: url,
+            base: base
+        });
+    }
+    onClient() {
+        setInterval(()=>{
+            this.state.count++;
+            const countEl = document.querySelector('#count');
+            if (countEl instanceof HTMLDivElement) {
+                countEl.innerText = String(this.state.count);
+            }
+        }, 1000);
+    }
+    /**
+     * 模拟服务端请求数据
+     */ async onServer() {
+        this.importMetaSet.add(import.meta);
+        super.onServer();
+        this.state.count = 1;
+        this.state.time = new Date().toISOString();
+    }
+    constructor(...args){
+        super(...args), _define_property(this, "state", {
+            count: 0,
+            time: ''
+        }), _define_property(this, "title", index_.title.home);
+    }
+}
+
+
+
+}),
+992: (function (module, __unused_webpack_exports, __webpack_require__) {
+module.exports = {};
+
+
+}),
+
+};
---
+++ ssr-vue2-host/importmap/973d63211202.final.mjs
@@ -0,0 +1,18 @@
+(() => {
+const base = document.currentScript.getAttribute("data-base");
+const importmap = {"imports":{"ssr-vue2-host\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-host\u002Fsrc\u002Fentry.client.ce705a98.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-remote\u002Fsrc\u002Fentry.client.60b13bfc.final.mjs","ssr-vue2-remote\u002Fvue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.fc7fb2bf.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.3137e5b1.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.fc7fb2bf.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.3137e5b1.final.mjs"},"scopes":{"\u002Fssr-vue2-host\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs"},"\u002Fssr-vue2-remote\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs"}}};
+const set = (data) => {
+    if (!data) return;
+    Object.entries(data).forEach(([k, v]) => {
+        data[k] = base + v;
+    });
+};
+set(importmap.imports);
+if (importmap.scopes) {
+    Object.values(importmap.scopes).forEach(set);
+}
+const script = document.createElement("script");
+script.type = "importmap";
+script.innerText = JSON.stringify(importmap);
+document.head.appendChild(script);
+})();
