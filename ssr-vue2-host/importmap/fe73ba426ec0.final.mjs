4bbbab98b7afb4da8fe26de04d92c788d382317a
lzxb <1340641314@qq.com>
2025-06-25 03:45:40 +0000

    Deploying to docs from @ esmnext/esmx@b8280a7227e48fb69f661c089b757f8f44f958a2 🚀

---
+++ ssr-vue2-host/importmap/fe73ba426ec0.final.mjs
@@ -0,0 +1,18 @@
+(() => {
+const base = document.currentScript.getAttribute("data-base");
+const importmap = {"imports":{"ssr-vue2-host\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-host\u002Fsrc\u002Fentry.client.f94d0821.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-remote\u002Fsrc\u002Fentry.client.2362ae14.final.mjs","ssr-vue2-remote\u002Fvue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.3ace8743.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.5de7aec1.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex.3ace8743.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables":"\u002Fssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex.fb511c82.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples":"\u002Fssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex.5de7aec1.final.mjs"},"scopes":{"\u002Fssr-vue2-host\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs"},"\u002Fssr-vue2-remote\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fvue.94c707fc.final.mjs"}}};
+const set = (data) => {
+    if (!data) return;
+    Object.entries(data).forEach(([k, v]) => {
+        data[k] = base + v;
+    });
+};
+set(importmap.imports);
+if (importmap.scopes) {
+    Object.values(importmap.scopes).forEach(set);
+}
+const script = document.createElement("script");
+script.type = "importmap";
+script.innerText = JSON.stringify(importmap);
+document.head.appendChild(script);
+})();
