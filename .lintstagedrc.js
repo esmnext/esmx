feeb2443e2fcf99289cc698bf19be121713117c3
Hanson <1340641314@qq.com>
2025-06-25 18:31:39 +0800

    chore(lint): remove unnecessary git add commands from lint-staged configuration
    - Updated the .lintstagedrc.js file to remove 'git add .' commands after linting CSS and JS files.
    - This change simplifies the linting process by ensuring that only linting commands are executed, improving clarity and maintainability.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 // Template generation, do not manually modify
 export default {
     '*.{css,vue}': (filenames) => {
-        return ['npm run lint:css', 'git add .'];
+        return ['npm run lint:css'];
     },
     '*.{ts,js,mjs,cjs}': (filenames) => {
-        return ['npm run lint:js', 'git add .'];
+        return ['npm run lint:js'];
     }
 };
