65580de8d789d3057c800298d4e3294e8109f362
jerrychan7 <czy-ken@163.com>
2025-06-25 11:07:15 +0800

    feat(router.demo): add dynamic spacing for JSON display and add indent line style

--- examples/router-demo-vue2/src/components/collapsible-json.vue
+++ examples/router-demo-vue2/src/components/collapsible-json.vue
@@ -1,5 +1,5 @@
 <template>
-    <pre v-html="vHTML"></pre>
+    <pre v-html="vHTML" :style="{'--space': space}"></pre>
 </template>
 
 <script lang="ts" setup>
@@ -7,10 +7,13 @@ import { computed } from 'vue';
 
 const props = defineProps<{
     data: any;
+    space?: number;
 }>();
 
+const space = props.space !== void 0 && props.space > 0 ? props.space : 2;
+
 const vHTML = computed(() =>
-    JSON.stringify(props.data, null, 2)
+    JSON.stringify(props.data, null, space)
         .replace(/^( *)(".*"):/gm, '$1<span class="json-key">$2</span>:')
         .replace(/: (".*")(,?)$/gm, ': <span class="json-str">$1</span>$2')
         .replace(
@@ -19,13 +22,14 @@ const vHTML = computed(() =>
         )
         .replace(/(: | +)(\d+)(,?)$/gm, '$1<span class="json-num">$2</span>$3')
         .replace(
-            /(^.*)([{[])$\n/gm,
-            `<details open
-            ><summary
-                >$1<span class="json-bracket $2">$2</span
-                ><span class="json-ellipsis"> ... </span
-            ></summary
-        ><div class="json-content">`
+            /(^ *)(.*)([{[])$\n/gm,
+            (all, spaces, content, bracket) =>
+                `<details${spaces.length ? ' open' : ''} style="--depth: ${spaces.length / space}"
+                    ><summary
+                        >${spaces}${content}<span class="json-bracket ${bracket}">${bracket}</span
+                        ><span class="json-ellipsis"> ... </span
+                    ></summary
+                ><div class="json-content">`.replace(/\n +>/g, '>')
         )
         .replace(
             /(^ *)([\]}])(,?)$\n?/gm,
@@ -77,6 +81,7 @@ pre {
 }
 
 :deep(.json-content) {
+    position: relative;
     padding-left: 2ch;
 }
 :deep(.json-key) {
@@ -103,4 +108,19 @@ pre {
 :deep(details[open] > summary .json-ellipsis) { display: none; }
 :deep(*).\{ + .json-ellipsis::after { content: '}'; color: var(--link-hover); font-weight: bold; }
 :deep(*).\[ + .json-ellipsis::after { content: ']'; color: var(--link-visited); font-weight: bold; }
+
+:deep(.json-content::after) {
+    content: '';
+    display: block;
+    height: calc(100% - 1.75em);
+    width: 0;
+    border-left: 1px dashed var(--border-color);
+    position: absolute;
+    left: calc(1ch * var(--space) * var(--depth) + 2ch + .4ch);
+    top: 0;
+    pointer-events: none;
+}
+:deep(details:hover:not(:has(details[open]:hover)) > .json-content::after) {
+    border-color: var(--text-tertiary);
+}
 </style>
