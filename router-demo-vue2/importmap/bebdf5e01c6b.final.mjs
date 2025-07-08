ae06c7969a811bde3fe27019822f7be738759c5c
lzxb <1340641314@qq.com>
2025-07-08 10:02:13 +0000

    Deploying to docs from @ esmnext/esmx@cf07daa8881856dfce985717203e975e1a4a5323 🚀

---
+++ router-demo-vue2/importmap/bebdf5e01c6b.final.mjs
@@ -0,0 +1,18 @@
+(() => {
+const base = document.currentScript.getAttribute("data-base");
+const importmap = {"imports":{"router-demo-vue2\u002Fsrc\u002Fentry.client":"\u002Frouter-demo-vue2\u002Fexports\u002Fsrc\u002Fentry.client.c6d49bd8.final.mjs","router-demo-vue2\u002Fvue":"\u002Frouter-demo-vue2\u002Fexports\u002Fvue.389c4cab.final.mjs","router-demo-vue2\u002F@esmx\u002Frouter":"\u002Frouter-demo-vue2\u002Fexports\u002F@esmx\u002Frouter.6e9fa025.final.mjs","router-demo-vue2\u002F@esmx\u002Frouter-vue":"\u002Frouter-demo-vue2\u002Fexports\u002F@esmx\u002Frouter-vue.3618af85.final.mjs"},"scopes":{"\u002Frouter-demo-vue2\u002F":{"vue":"\u002Frouter-demo-vue2\u002Fexports\u002Fvue.389c4cab.final.mjs","@esmx\u002Frouter":"\u002Frouter-demo-vue2\u002Fexports\u002F@esmx\u002Frouter.6e9fa025.final.mjs","@esmx\u002Frouter-vue":"\u002Frouter-demo-vue2\u002Fexports\u002F@esmx\u002Frouter-vue.3618af85.final.mjs"}}};
+const set = (data) => {
+    if (!data) return;
+    Object.entries(data).forEach(([k, v]) => {
+        data[k] = base + v;
+    });
+};
+set(importmap.imports);
+if (importmap.scopes) {
+    Object.values(importmap.scopes).forEach(set);
+}
+const script = document.createElement("script");
+script.type = "importmap";
+script.innerText = JSON.stringify(importmap);
+document.head.appendChild(script);
+})();
