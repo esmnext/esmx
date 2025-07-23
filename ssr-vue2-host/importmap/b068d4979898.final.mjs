225ae913f465bb7c81bbd48f7c0df0345f623035
lzxb <1340641314@qq.com>
2025-07-23 10:02:47 +0000

    Deploying to docs from @ esmnext/esmx@a3ae539117bd523e44e02e71b95a948bad19815a 🚀

---
+++ ssr-vue2-host/importmap/b068d4979898.final.mjs
@@ -0,0 +1,18 @@
+(() => {
+const base = document.currentScript.getAttribute("data-base");
+const importmap = {"imports":{"ssr-vue2-host\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-host\u002Fexports\u002Fsrc\u002Fentry.client.8351ec70.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fentry.client":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fentry.client.ace3b569.final.mjs","ssr-vue2-remote\u002Fvue":"\u002Fssr-vue2-remote\u002Fexports\u002Fvue.389c4cab.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents\u002Findex":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fcomponents\u002Findex.ae0da56c.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables\u002Findex":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fcomposables\u002Findex.c2bfa8f0.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples\u002Findex":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fexamples\u002Findex.fda86f0a.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomponents":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fcomponents\u002Findex.ae0da56c.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fcomposables":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fcomposables\u002Findex.c2bfa8f0.final.mjs","ssr-vue2-remote\u002Fsrc\u002Fexamples":"\u002Fssr-vue2-remote\u002Fexports\u002Fsrc\u002Fexamples\u002Findex.fda86f0a.final.mjs"},"scopes":{"\u002Fssr-vue2-host\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fexports\u002Fvue.389c4cab.final.mjs"},"\u002Fssr-vue2-remote\u002F":{"vue":"\u002Fssr-vue2-remote\u002Fexports\u002Fvue.389c4cab.final.mjs"}}};
+const set = (data) => {
+    if (!data) return;
+    Object.entries(data).forEach(([k, v]) => {
+        data[k] = base + v;
+    });
+};
+set(importmap.imports);
+if (importmap.scopes) {
+    Object.values(importmap.scopes).forEach(set);
+}
+const script = document.createElement("script");
+script.type = "importmap";
+script.innerText = JSON.stringify(importmap);
+document.head.appendChild(script);
+})();
