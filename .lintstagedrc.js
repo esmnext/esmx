7422b4b71b6a1465e2d67df7b4f09ce91a967f1a
Hanson <1340641314@qq.com>
2025-07-30 20:20:34 +0800

    chore: update linting configuration and package dependencies
    - Consolidated linting commands in .lintstagedrc.js to run JavaScript linting for all relevant file types.
    - Updated biome.json files across multiple packages to disable 'useGetterReturn' warnings and enable CSS formatter and linter.
    - Adjusted package.json scripts to replace stylelint commands with JavaScript linting.
    - Cleaned up pnpm-lock.yaml by removing unused dependencies.
    - Standardized CSS variable color values to lowercase in various stylesheets for consistency.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,4 @@
 // Template generation, do not manually modify
 export default {
-    '*.{css,vue}': (filenames) => {
-        return ['npm run lint:css', 'git add .'];
-    },
-    '*.{ts,js,mjs,cjs}': (filenames) => {
-        return ['npm run lint:js', 'git add .'];
-    }
+    '*.{ts,js,mjs,cjs,vue,css,less}': ['npm run lint:js', 'git add .']
 };
